82a1f65da4296853aa120e9487db51659525e336
“himekoh” <h2tafemin@gmail.com>
2022-10-27 17:39:28 +0900

    delete SECRET_KEY

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ghc0_2q^b5_+023)8$m%r2!f5n-p=521p2i1k&!_&eon8_bx=c'
+# SECRET_KEY = 'django-insecure-ghc0_2q^b5_+023)8$m%r2!f5n-p=521p2i1k&!_&eon8_bx=c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
